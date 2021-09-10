fba4b32d26e4928fef0ca17aba246699841e2157
Aubri Henley <aubri.henley@gmail.com>
2021-09-10 12:15:08 -0400

    updated associations in models index.js

--- models/index.js
+++ models/index.js
@@ -6,16 +6,15 @@ Post.hasMany(Comment, {
   foreignKey: 'post_id',
 });
 
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+})
+
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
 });
 
-User.hasMany(Comment,{
-  foreignKey:'user_id',
-});
 
-User.hasMany(Post, {
-  foreignKey:'user_id',
-});
 
 module.exports = { User, Blog, Post, Comment };
